d18f33ddc8f51b8278c560b8362290acebec1849
David Coleman <davidmcoleman@me.com>
2022-01-26 17:47:56 -0500

    removed globals.js file

--- src/globals.js
+++
@@ -1,4 +0,0 @@
-//export const API_URL = 'https://api.inaturalist.org/v1/taxa?q=';
-
-//birds at Waterman
-export const API_URL = 'https://api.inaturalist.org/v1/observations?lat=42.081483&lng=-76.168993&radius=5&iconic_taxa=Aves&taxon_summary';
